103ad70626a5ee7a99d5873f33f6b28c60ddf157
Trino <74266244+Trino11@users.noreply.github.com>
2023-01-19 00:31:48 +0100

    Update startup.sql

--- startup.sql
+++ startup.sql
@@ -52,5 +52,5 @@ CREATE TABLE `userssubscribedinstances` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `roles`(`id`,`name`,`permissions`,`admin`)VALUES('1', 'admin', '[\"login\", \"user-show\", \"user-show-all\", \"user-create\", \"user-update\", \"user-update-all\", \"instance-show-subscribed\", \"instance-show-other-subscribed\", \"instance-show-all\", \"instance-show-admin\", \"instance-subscribe\", \"instance-subscribe-admin\", \"instance-other-subscribe\", \"instance-other-subscribe-admin\", \"user-delete-all\"]', '1');
-INSERT INTO `users`(`id`,`username`,`password`,`token`)VALUES('1', 'root', '$2b$10$vmn.TjXf0mlZsppXKfSCSe/zkk7tu/H5uU.ciN.EYvRG7WT7yj1fq', NULL);
+INSERT INTO `users`(`id`,`username`,`password`,`token`)VALUES('1', 'root', '', NULL);
 INSERT INTO `userxroles`(`userId`,`roleId`)VALUES('1', '1');
